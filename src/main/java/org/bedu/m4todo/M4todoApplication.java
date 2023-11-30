2b7add9f1221539ecec74b41f6775a7359bdbd1e
jvergara99 <jvergara@uv.mx>
2023-11-30 12:33:57 -0600

    Initial commit

---
+++ src/main/java/org/bedu/m4todo/M4todoApplication.java
@@ -0,0 +1,13 @@
+package org.bedu.m4todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M4todoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M4todoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bedu/m4todo/M4todoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.m4todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M4todoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
